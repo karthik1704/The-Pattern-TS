0f5662c12647ab0115e284f858c639b9c7dc3668
Karthik A <karthikthee7@gmail.com>
2021-05-29 19:10:56 +0530

    small Changes

--- src/helper/axiosInterceptor.ts
+++ src/helper/axiosInterceptor.ts
@@ -11,8 +11,8 @@ const myAxios = axios.create({
 myAxios.interceptors.request.use(
     async (config) => {
         const token = await window.localStorage.getItem('accesstoken');
-        if (token){
-            config.headers.Authorization=`Bearer ${token}`;
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
         }
         return config;
     },
