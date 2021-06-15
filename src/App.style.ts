62d53150c14720098c04d2c5319bd99d23d3ffb9
Karthik A <karthikthee7@gmail.com>
2021-06-15 08:34:39 +0530

    change name app.css to app.style.ts

---
+++ src/App.style.ts
@@ -0,0 +1,8 @@
+import { createStyles, makeStyles, Theme } from "@material-ui/core";
+
+
+export default makeStyles((theme:Theme)=>createStyles({
+    root:{
+        minHeight: '100vh',
+    }
+}));
