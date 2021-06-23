0c62ebd1638bed76ca615d30dc15f36b5f2c10e3
Karthik A <karthikthee7@gmail.com>
2021-06-23 20:25:19 +0530

    Add footer Contents

--- src/components/common/Footer/Footer.style.ts
+++ src/components/common/Footer/Footer.style.ts
@@ -4,6 +4,9 @@ import { createStyles, makeStyles, Theme } from "@material-ui/core";
 export default makeStyles((theme:Theme)=>createStyles({
     root:{
         display: 'flex',
-        
+        borderTop: `1px solid ${theme.palette.divider}`,
+        marginTop: theme.spacing(1),
+        paddingTop: theme.spacing(3),
+        paddingBottom: theme.spacing(3),
     }
 }));
--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -1,23 +1,198 @@
-import { FC, ReactElement } from "react";
-import { APP_NAME } from "../../../constants/base";
+import { FC, ReactElement } from 'react';
+import { APP_NAME } from '../../../constants/base';
 
-import Typography from "@material-ui/core/Typography";
+import Container from '@material-ui/core/Container';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { Box, Grid } from '@material-ui/core';
+import { Button, Link } from '@material-ui/core';
+
+import { Link as RouterLink, LinkProps as RouterLinkProps } from 'react-router-dom';
+
+// Icons
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import RedditIcon from '@material-ui/icons/Reddit';
 
 import useStyles from './Footer.style';
+import useToggleTheme from '../../../hooks/useToggleTheme';
 
-const Footer:FC = ():ReactElement => {
+interface QLinks extends RouterLinkProps {
+    name: string;
+    
+}
+
+const quickLinks: QLinks[] = [
+    {
+        name: 'About',
+        to: '/about',
+    },
+    {
+        name: 'Contact',
+        to: '/contact',
+    },
+    {
+        name: 'Privacy & Policy',
+        to: '/privacy',
+    },
+
+];
+
+const Footer: FC = (Props): ReactElement => {
+    const [theme,setTheme] = useToggleTheme();
     const classes = useStyles();
+    console.log('Props', Props)
+
+    const onHandleDarkMode = ()=>{
+        if (theme==='light'){
+            setTheme('dark')
+        } else{
+            setTheme('light')
+        }
+    }
 
     return (
-        <div className={classes.root}>
-            <div>
-                <Typography variant="h6" noWrap >
-                        {APP_NAME}
-                </Typography>
-            </div>
+        <>
+            <Container
+                maxWidth="lg"
+                component="footer"
+                className={classes.root}
+            >
+                <Grid container spacing={4} justify="space-evenly">
+                    <Grid item xs={12} md={3}>
+                        <Link
+                            variant="h6"
+                            noWrap
+                            color="textPrimary"
+                            gutterBottom
+                            underline='none'
+                            component={RouterLink}
+                            to='/'
+                        >
+                            {APP_NAME}
+                        </Link>
+                        <Typography variant="subtitle2" color="textSecondary">
+                            Check out the hand-picked collection of latest
+                            mobile design patternsfrom apps that reflect the
+                            best in design.
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} md={3}>
+                        <Typography
+                            variant="h6"
+                            component="p"
+                            color="textPrimary"
+                            gutterBottom
+                            noWrap
+                        >
+                            Quick Link
+                        </Typography>
+                        <Box
+                            display="flex"
+                            justifyContent="space-between"
+                            flexDirection='column'
+                            flexWrap="warp"
+                        >
+                            {quickLinks.map((link) => (
+                                <Link
+                                component={RouterLink} to={link.to}
+                                key={link.name}
+                                color='textSecondary'
+                                variant='subtitle2'
+                                >
+                                    {link.name}
+                                </Link>
+                            ))}
+                            <Link
+                                color='textSecondary'
+                                variant='subtitle2'
+                                onClick = {onHandleDarkMode}
+                                >
+                                    Dark Mode
+                                </Link>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} md={3}>
+                        <Typography
+                            variant="h6"
+                            component="p"
+                            color="textPrimary"
+                            gutterBottom
+                            noWrap
+                        >
+                            Social
+                        </Typography>
+                        <IconButton
+                            href="https://instagram.com/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <InstagramIcon />
+                        </IconButton>
+                        <IconButton
+                            href="https://fb.com/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <FacebookIcon />
+                        </IconButton>
+                        <IconButton
+                            href="https://twitter.com/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <TwitterIcon />
+                        </IconButton>
+                        <IconButton
+                            href="https://reddit.com/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <RedditIcon />
+                        </IconButton>
+                    </Grid>
+                    <Grid item xs={12} md={3}>
+                        <Typography
+                            variant="h6"
+                            component="p"
+                            color="textPrimary"
+                            gutterBottom
+                            noWrap
+                        >
+                            Donate
+                        </Typography>
 
-        </div>
-    )
-}
+                        <Typography
+                            variant="subtitle2"
+                            color="textSecondary"
+                            gutterBottom
+                        >
+                            Please support our page to give more valuable
+                            content.
+                        </Typography>
+                        <Button variant="contained" color="primary" component={RouterLink} to='/donate'>
+                            Donate Now
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Container>
+            <Box mt={5} pb={2}>
+                <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    align="center"
+                >
+                    {'© '}
+                    {new Date().getFullYear()}
+                    {'  Crafted by '}
+                    <Link color="inherit" href="https://material-ui.com/">
+                        {APP_NAME} Team.
+                    </Link>
+                </Typography>
+            </Box>
+        </>
+    );
+};
 
 export default Footer;
