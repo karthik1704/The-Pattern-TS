2e9b888ae071cb033374c7d8bdb7c6352d6e4501
Karthik A <karthikthee7@gmail.com>
2021-07-05 08:55:54 +0530

    create custom base query --> axiosBaseQuery

---
+++ src/helper/axiosBaseQuery.ts
@@ -0,0 +1,32 @@
+
+import myAxios from "./axiosInterceptor";
+
+
+import { BaseQueryFn } from '@reduxjs/toolkit/query'
+import { AxiosRequestConfig, AxiosError } from 'axios'
+
+const axiosBaseQuery =
+  (
+    { baseUrl }: { baseUrl: string } = { baseUrl: '' }
+  ): BaseQueryFn<
+    {
+      url: string
+      method: AxiosRequestConfig['method']
+      data?: AxiosRequestConfig['data']
+    },
+    unknown,
+    unknown
+  > =>
+  async ({ url, method, data }) => {
+    try {
+      const result = await myAxios({ url: baseUrl + url, method, data })
+      return { data: result.data }
+    } catch (axiosError) {
+      let err = axiosError as AxiosError
+      return {
+        error: { status: err.response?.status, data: err.response?.data },
+      }
+    }
+  }
+
+export default axiosBaseQuery;
--- src/helper/axiosInterceptor.ts
+++ src/helper/axiosInterceptor.ts
@@ -1,10 +1,8 @@
 import axios from 'axios';
-import { API_URL } from '../constants/base';
 
 let headers = {};
 
 const myAxios = axios.create({
-    baseURL: API_URL,
     headers,
 });
 
