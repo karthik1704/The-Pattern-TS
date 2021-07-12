a4f095df1f7ebddd169f07721bd71942715479d9
Karthik A <karthikthee7@gmail.com>
2021-07-12 21:20:33 +0530

    add basic imports and state

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,15 +1,21 @@
 import { TextField } from '@material-ui/core';
-import {FC, ReactElement} from 'react';
+import { FC, ReactElement, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useLoginMutation } from '../../features/auth/authApi';
 
+const Login: FC = (): ReactElement => {
+    const [loginState, setLoginState] = useState({
+        email: '',
+        password: '',
+    });
+    const { push } = useHistory();
 
-const Login:FC = ():ReactElement => {
-    return(
+    const [login, { isLoading }] = useLoginMutation();
+    return (
         <div>
-            <TextField>
-                
-            </TextField>
+            <TextField></TextField>
         </div>
-    )
-}
+    );
+};
 
 export default Login;
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,7 +1,20 @@
-import {FC, ReactElement} from 'react';
+import { useState, FC, ReactElement } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useRegisterMutation } from '../../features/auth/authApi';
 
-const Register:FC = ():ReactElement =>{
-    return <p>Register</p>
-}
+const Register: FC = (): ReactElement => {
+    const [registerState, setRegisterState] = useState({
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+        password1: '',
+    });
+    const { push } = useHistory();
+
+    const [register, { isLoading }] = useRegisterMutation();
+
+    return <p>Register</p>;
+};
 
 export default Register;
