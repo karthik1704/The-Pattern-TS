5b508d5d10d85f547749718e3aa438db23e9adf4
Karthik A <karthikthee7@gmail.com>
2021-07-23 21:57:40 +0530

    added to more impots for card

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,10 +1,15 @@
-import { Card } from '@material-ui/core';
 import { FC, ReactElement } from 'react';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 
 const ProjectCard: FC = (): ReactElement => {
     return (
         <Card>
-            <Card></Card>
+            <CardMedia />
+            <CardContent>HI</CardContent>
+            <CardActions></CardActions>
         </Card>
     );
 };
