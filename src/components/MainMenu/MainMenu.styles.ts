0ad152013a14147dbdadcf43850183b71539b321
Karthik A <karthikthee7@gmail.com>
2021-07-28 18:54:59 +0530

    Commented style JSS it causing TS Error

--- src/components/MainMenu/MainMenu.styles.ts
+++ src/components/MainMenu/MainMenu.styles.ts
@@ -1,14 +1,14 @@
 import { makeStyles, createStyles } from '@material-ui/styles';
 
-export default makeStyles((theme) =>
-    createStyles({
-        avatar: {
-            display: 'flex',
-            //alignItems: 'center',
-            flexDirection: 'column',
-            '&>*': {
-                margin: theme.spacing(1),
-            },
-        },
-    })
-);
+// export default makeStyles((theme) =>
+//     createStyles({
+//         avatar: {
+//             display: 'flex',
+//             //alignItems: 'center',
+//             flexDirection: 'column',
+//             '&>*': {
+//                 margin: theme.spacing(1),
+//             },
+//         },
+//     })
+// );
--- src/components/common/Footer/Footer.style.ts
+++ src/components/common/Footer/Footer.style.ts
@@ -1,13 +1,13 @@
 import { createStyles, makeStyles } from '@material-ui/styles';
 
-export default makeStyles((theme) =>
-    createStyles({
-        root: {
-            display: 'flex',
-            borderTop: `1px solid ${theme.palette.divider}`,
-            marginTop: theme.spacing(1),
-            paddingTop: theme.spacing(3),
-            paddingBottom: theme.spacing(3),
-        },
-    })
-);
+// export default makeStyles((theme) =>
+//     createStyles({
+//         root: {
+//             display: 'flex',
+//             borderTop: `1px solid ${theme.palette.divider}`,
+//             marginTop: theme.spacing(1),
+//             paddingTop: theme.spacing(3),
+//             paddingBottom: theme.spacing(3),
+//         },
+//     })
+// );
