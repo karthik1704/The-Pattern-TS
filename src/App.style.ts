22c5da6963fa57725938a961c00387f9bf1cbb3c
Karthik A <karthikthee7@gmail.com>
2021-09-09 16:54:08 +0530

    removed mui/styles and styles files

--- src/App.style.ts
+++
@@ -1,9 +0,0 @@
-import { styled } from '@material-ui/core/styles';
-
-// export default makeStyles((theme) =>
-//     createStyles({
-//         root: {
-//             minHeight: '100vh',
-//         },
-//     })
-// );
--- src/components/ProjectCard/ProjectCard.styles.ts
+++
@@ -1,3 +0,0 @@
-import { makeStyles, createStyles } from '@material-ui/styles';
-
-export default makeStyles((theme) => createStyles({}));
