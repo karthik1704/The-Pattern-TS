2a3b20b5d780d6832daea6da683618ec677336fa
Karthik A <karthikthee7@gmail.com>
2021-10-11 20:01:49 +0530

    add response intercerptors to handle jwt refresh

--- src/helper/axiosInterceptor.ts
+++ src/helper/axiosInterceptor.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { logoutUser, refreshAccessToken } from '../features/auth/authSlice';
 
 let headers = {};
 
@@ -8,7 +9,7 @@ const myAxios = axios.create({
 
 myAxios.interceptors.request.use(
     async (config) => {
-        const token = await window.localStorage.getItem('accesstoken');
+        const token = await localStorage.getItem('accesstoken');
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
@@ -17,6 +18,37 @@ myAxios.interceptors.request.use(
     (error) => {
         console.error('Axios Error -> ', JSON.stringify(error));
         return Promise.reject(error);
+    }
+);
+
+myAxios.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        const originalRequest = error.config;
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            axios
+                .post(`${process.env.REACT_APP_API_URL}/auth/token/refresh`, {
+                    refresh: localStorage.getItem('refresh_token'),
+                })
+                .then((res) => {
+                    const access_token = res.data.access;
+                    refreshAccessToken(access_token);
+                    axios.defaults.headers.common[
+                        'Authorization'
+                    ] = `Bearer ${access_token}`;
+                    originalRequest.heraders[
+                        'Authorization'
+                    ] = `Bearer ${access_token}`;
+                    return myAxios(originalRequest);
+                })
+                .catch((error) => {
+                    console.log('Axios Refersh token error code:', error);
+                    logoutUser(false);
+                });
+
+            return Promise.reject(error);
+        }
     }
 );
 
