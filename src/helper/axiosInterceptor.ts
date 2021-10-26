85fe406dcc038ffe4fed10a922ab714385ad4f4a
Karthik A <karthikthee7@gmail.com>
2021-10-26 19:49:01 +0530

    add support refresh jwt in 403 error

--- src/helper/axiosInterceptor.ts
+++ src/helper/axiosInterceptor.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { logoutUser, refreshAccessToken } from '../features/auth/authSlice';
 
+import store from '../features/store';
+
 let headers = {};
 
 const myAxios = axios.create({
@@ -9,7 +11,7 @@ const myAxios = axios.create({
 
 myAxios.interceptors.request.use(
     async (config) => {
-        const token = await localStorage.getItem('accesstoken');
+        const token = await localStorage.getItem('access_token');
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
@@ -23,28 +25,39 @@ myAxios.interceptors.request.use(
 
 myAxios.interceptors.response.use(
     (response) => response,
-    (error) => {
-        const originalRequest = error.config;
-        if (
-            error.response.status === 401 &&
-            !originalRequest._retry &&
-            originalRequest.url !==
-                `${process.env.REACT_APP_API_URL}/auth/token/refresh`
-        ) {
+    async (error) => {
+        const originalRequest = await error.config;
+        console.log(error.response);
+        console.log(originalRequest);
+        console.log(originalRequest._retry);
+        console.log(error.response.status === 401);
+        const t = localStorage.getItem('refresh_token');
+        if (t) {
+            const tp = JSON.parse(atob(t?.split('.')[1]));
+            const now = Math.ceil(Date.now() / 1000);
+            console.log(tp);
+            console.log(tp.exp > now);
+        }
+
+        if (error.response.status === 401 && !originalRequest._retry) {
             const refreshToken = localStorage.getItem('refresh_token');
+            console.log(refreshToken);
             originalRequest._retry = true;
             if (refreshToken) {
                 axios
                     .post(
-                        `${process.env.REACT_APP_API_URL}/auth/token/refresh`,
+                        `${process.env.REACT_APP_API_URL}auth/token/refresh/`,
                         {
                             refresh: refreshToken,
                         }
                     )
                     .then((res) => {
                         const access_token = res.data.access;
-                        refreshAccessToken(access_token);
-                        myAxios.defaults.headers.common[
+                        console.log('im here');
+                        console.log('access:', access_token);
+                        localStorage.setItem('access_token', access_token);
+                        store.dispatch(refreshAccessToken({ access_token }));
+                        myAxios.defaults.headers[
                             'Authorization'
                         ] = `Bearer ${access_token}`;
                         originalRequest.headers[
@@ -54,13 +67,15 @@ myAxios.interceptors.response.use(
                     })
                     .catch((error) => {
                         console.log('Axios Refersh token error code:', error);
-                        logoutUser(false);
+                        store.dispatch(logoutUser(false));
+                        delete myAxios.defaults.headers.common['Authorization'];
                         return Promise.reject(error);
                     });
             } else {
-                logoutUser(false);
+                store.dispatch(logoutUser(false));
             }
         }
+
         return Promise.reject(error);
     }
 );
