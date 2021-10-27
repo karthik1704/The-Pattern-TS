d3264265c1664055d4ce8ed0ebf18c13e7728417
Karthik A <karthikthee7@gmail.com>
2021-10-27 18:42:31 +0530

    add card in myBoards

--- src/pages/MyBoards/index.tsx
+++ src/pages/MyBoards/index.tsx
@@ -2,6 +2,9 @@ import { FC, ReactElement } from 'react';
 
 // MUI
 import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 import { useGetBoardsQuery } from '../../features/myBoards/myBoardsApi';
 
@@ -11,9 +14,20 @@ const MyBoards: FC = (): ReactElement => {
     return (
         <>
             <Typography variant="h4">Myboards</Typography>
+            <Divider />
             {data &&
                 data?.results?.map((board) => (
-                    <p key={board.id}>{board.name}</p>
+                    <Card key={board.slug} sx={{ maxWidth: 250 }}>
+                        <CardContent
+                            sx={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                        >
+                            {board.name}
+                        </CardContent>
+                    </Card>
                 ))}
         </>
     );
