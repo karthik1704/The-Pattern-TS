b1efdde1fc2ed7b7dfe285aee88bb2ff2d4e3f7a
Karthik A <karthikthee7@gmail.com>
2021-11-11 21:41:51 +0530

    add improvements

--- src/pages/ProjectDetail/index.tsx
+++ src/pages/ProjectDetail/index.tsx
@@ -1,7 +1,21 @@
-import { FC, ReactElement } from "react";
+import { FC, ReactElement } from 'react';
 
-const ProjectDetail:FC = ():ReactElement => {
-    return <p>ProjectDetail</p>
-}
+import { useParams } from 'react-router-dom';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+
+const ProjectDetail: FC = (): ReactElement => {
+    const { slug } = useParams();
+    return <p>ProjectDetail- {slug}</p>;
+};
 
 export default ProjectDetail;
