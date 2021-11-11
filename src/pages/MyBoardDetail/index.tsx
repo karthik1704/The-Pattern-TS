d2f3110e2407d1cc670c642c2cc4aac31ac941b0
Karthik A <karthikthee7@gmail.com>
2021-11-11 21:41:33 +0530

    add imports

--- src/pages/MyBoardDetail/index.tsx
+++ src/pages/MyBoardDetail/index.tsx
@@ -1,7 +1,45 @@
-import { FC, ReactElement } from "react";
+import { FC, ReactElement } from 'react';
 
-const MyBoardDetail:FC = ():ReactElement=>{
-    return <p>Detail Board</p>
-}
+import { useParams } from 'react-router-dom';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+
+import DeleteIcon from '@mui/icons-material/Delete';
+
+import { useGetBoardDetailQuery } from '../../features/myBoards/myBoardsApi';
+
+import Helmet from 'react-helmet';
+import { APP_NAME } from '../../constants/base';
+
+const MyBoardDetail: FC = (): ReactElement => {
+    const { slug } = useParams();
+    const { data: board, isFetching } = useGetBoardDetailQuery(slug!);
+
+    return (
+        <>
+            <Helmet>
+                <title>
+                    {slug} Detail - {APP_NAME}
+                </title>
+            </Helmet>
+            <Box>
+                {board && (
+                    <Container sx={{ justifyContent: 'space-between' }}>
+                        <Typography variant="h4">{board.board_name}</Typography>
+                    </Container>
+                )}
+            </Box>
+        </>
+    );
+};
 
 export default MyBoardDetail;
