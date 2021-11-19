ad29029186df44a92ab88f474ce82915d7d14565
Karthik A <karthikthee7@gmail.com>
2021-11-19 22:06:16 +0530

    create Delete Board

---
+++ src/components/DeleteBoard/DeleteBoard.tsx
@@ -0,0 +1,47 @@
+import { FC, ReactElement } from 'react';
+
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+import { useDeleteBoardMutation } from '../../features/myBoards/myBoardsApi';
+import { useNavigate } from 'react-router';
+
+interface Props {
+    open: boolean;
+    handleClose: () => void;
+    slug?: string;
+}
+
+const DeleteBoard: FC<Props> = ({ open, handleClose, slug }): ReactElement => {
+    const [deleteBoard, { isLoading }] = useDeleteBoardMutation();
+    let navigate = useNavigate();
+    const handleDeleteBoard = () => {
+        deleteBoard(slug as string);
+        navigate('..', { replace: true });
+    };
+    return (
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">
+                {'Do you want to  Delete?'}
+            </DialogTitle>
+
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button color="error" onClick={handleDeleteBoard} autoFocus>
+                    Delete
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+export default DeleteBoard;
