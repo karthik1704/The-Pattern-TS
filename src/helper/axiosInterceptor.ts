16c51da438fdf078b65abafc4900fde8893ca7c0
Karthik A <karthikthee7@gmail.com>
2021-12-20 09:27:43 +0530

    system switch

--- src/helper/axiosInterceptor.ts
+++ src/helper/axiosInterceptor.ts
@@ -3,18 +3,24 @@ import { logoutUser, refreshAccessToken } from '../features/auth/authSlice';
 
 import store from '../features/store';
 
-let headers = {};
+let headers = {
+    // 'Access-Control-Allow-Origin': '*',
+};
 
 const myAxios = axios.create({
     headers,
 });
 
+myAxios.defaults.withCredentials = true;
+// myAxios.defaults.headers['Access-Control-Allow-Credentials'] = true;
+
 myAxios.interceptors.request.use(
     async (config) => {
         const token = await localStorage.getItem('access_token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
+
+        // if (token) {
+        //     config.headers.Authorization = `Bearer ${token}`;
+        // }
         return config;
     },
     (error) => {
@@ -26,55 +32,55 @@ myAxios.interceptors.request.use(
 myAxios.interceptors.response.use(
     (response) => response,
     async (error) => {
-        const originalRequest = await error.config;
-        console.log(error.response);
-        console.log(originalRequest);
-        console.log(originalRequest._retry);
-        console.log(error.response.status === 401);
-        const t = localStorage.getItem('refresh_token');
-        if (t) {
-            const tp = JSON.parse(atob(t?.split('.')[1]));
-            const now = Math.ceil(Date.now() / 1000);
-            console.log(tp);
-            console.log(tp.exp > now);
-        }
+        // const originalRequest = await error.config;
+        // console.log(error.response);
+        // console.log(originalRequest);
+        // console.log(originalRequest._retry);
+        // console.log(error.response.status === 401);
+        // const t = localStorage.getItem('refresh_token');
+        // if (t) {
+        //     const tp = JSON.parse(atob(t?.split('.')[1]));
+        //     const now = Math.ceil(Date.now() / 1000);
+        //     console.log(tp);
+        //     console.log(tp.exp > now);
+        // }
 
-        if (error.response.status === 401 && !originalRequest._retry) {
-            const refreshToken = localStorage.getItem('refresh_token');
-            console.log(refreshToken);
-            originalRequest._retry = true;
-            if (refreshToken) {
-                axios
-                    .post(
-                        `${process.env.REACT_APP_API_URL}auth/token/refresh/`,
-                        {
-                            refresh: refreshToken,
-                        }
-                    )
-                    .then((res) => {
-                        const access_token = res.data.access;
-                        console.log('im here');
-                        console.log('access:', access_token);
-                        localStorage.setItem('access_token', access_token);
-                        store.dispatch(refreshAccessToken({ access_token }));
-                        myAxios.defaults.headers[
-                            'Authorization'
-                        ] = `Bearer ${access_token}`;
-                        originalRequest.headers[
-                            'Authorization'
-                        ] = `Bearer ${access_token}`;
-                        return myAxios(originalRequest);
-                    })
-                    .catch((error) => {
-                        console.log('Axios Refersh token error code:', error);
-                        store.dispatch(logoutUser(false));
-                        delete myAxios.defaults.headers.common['Authorization'];
-                        return Promise.reject(error);
-                    });
-            } else {
-                store.dispatch(logoutUser(false));
-            }
-        }
+        // if (error.response.status === 401 && !originalRequest._retry) {
+        //     const refreshToken = localStorage.getItem('refresh_token');
+        //     console.log(refreshToken);
+        //     originalRequest._retry = true;
+        //     if (refreshToken) {
+        //         axios
+        //             .post(
+        //                 `${process.env.REACT_APP_API_URL}auth/token/refresh/`,
+        //                 {
+        //                     refresh: refreshToken,
+        //                 }
+        //             )
+        //             .then((res) => {
+        //                 const access_token = res.data.access;
+        //                 console.log('im here');
+        //                 console.log('access:', access_token);
+        //                 localStorage.setItem('access_token', access_token);
+        //                 store.dispatch(refreshAccessToken({ access_token }));
+        //                 myAxios.defaults.headers[
+        //                     'Authorization'
+        //                 ] = `Bearer ${access_token}`;
+        //                 originalRequest.headers[
+        //                     'Authorization'
+        //                 ] = `Bearer ${access_token}`;
+        //                 return myAxios(originalRequest);
+        //             })
+        //             .catch((error) => {
+        //                 console.log('Axios Refersh token error code:', error);
+        //                 store.dispatch(logoutUser(false));
+        //                 delete myAxios.defaults.headers.common['Authorization'];
+        //                 return Promise.reject(error);
+        //             });
+        //     } else {
+        //         store.dispatch(logoutUser(false));
+        //     }
+        // }
 
         return Promise.reject(error);
     }
